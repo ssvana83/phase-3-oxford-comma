b5802e8f6ffd9777b82d0ecfde3b56c83d96db53
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-02-14 15:23:58 -0500

    DONE

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array[0...-1].join(", ") << ", and #{array[-1]}"
+    else
+      array.join
+    end      
 end
